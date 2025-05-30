a998fa6d39408effc8ae558e8459d3fa7fa39936
Mashhoud Rajput <mashhoud.ashraf@nextgeni.com>
2025-05-30 19:23:59 +0500

    add ssh plugin and make full dynamic

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Replace placeholders in config with environment variables
-sed -i "s/__ALLOWED_HOSTS__/${ALLOWED_HOSTS:-52.208.159.63}/g" /etc/nagios/nrpe.cfg.template
-mv /etc/nagios/nrpe.cfg.template /etc/nagios/nrpe.cfg
+sed -i "s/__ALLOWED_HOSTS__/${ALLOWED_HOSTS:-52.208.159.63}/g" /etc/nagios/nrpe.cfg
+mv /etc/nagios/nrpe.cfg /etc/nagios/nrpe.cfg
 
 echo "Starting NRPE agent for Nagios server: ${ALLOWED_HOSTS}"
 exec /usr/bin/nrpe -f -c /etc/nagios/nrpe.cfg
