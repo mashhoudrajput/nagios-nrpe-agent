0085305a40ad940c58b6ed2403987903e0e0fa32
SyedMehrab <s.mehrab@nextgeni.com>
2025-06-13 05:36:03 +0000

    update image; make dynamic add allowed host in -e

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 set -e
 
-# Replace placeholders in config with environment variables
-sed -i "s/__ALLOWED_HOSTS__/${ALLOWED_HOSTS:-52.208.159.63}/g" /etc/nagios/nrpe.cfg
-mv /etc/nagios/nrpe.cfg /etc/nagios/nrpe.cfg
+# Set allowed hosts, fallback to default if not provided
+ALLOWED_HOSTS="${ALLOWED_HOSTS:-52.208.159.63}"
+
+# Replace placeholder in the config
+sed -i "s/__ALLOWED_HOSTS__/${ALLOWED_HOSTS}/g" /etc/nagios/nrpe.cfg
 
 echo "Starting NRPE agent for Nagios server: ${ALLOWED_HOSTS}"
+
+# Start NRPE
 exec /usr/bin/nrpe -f -c /etc/nagios/nrpe.cfg
+
